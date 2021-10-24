c47fb72cb8ef7099332fb02a60d05527ea19e4d2
AxelBlackXZ07 <92955733+AxelBlackXZ07@users.noreply.github.com>
2021-10-24 05:14:52 -0300

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'BlackBot',
-        ownerName: 'Axelin',
+        botName: 'ShanBot',
+        ownerName: 'Shan',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
